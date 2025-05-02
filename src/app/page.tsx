5e8c5cf3cddc56e4fae68f23d17b35689431f8a4
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-05-02 23:19:24 +0900

    API修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,28 @@
 export default async function Home() {
-  const data = await fetch("https://jsonplaceholder.typicode.com/posts/");
-  const posts = await data.json();
+  const data = await fetch("https://api.football-data.org/v4/teams/64", {
+    headers: {
+      "X-Auth-Token": "d9b12d4244034c36a3ae87539c71b59a",
+    },
+  });
 
-  type Post = {
-    id: number;
-    title: string;
-    body: string;
-  };
+  const posts = await data.json();
+  console.log(posts);
+
+  // type Post = {
+  //   id: number;
+  //   title: string;
+  //   body: string;
+  // };
 
   return (
     <div>
       <h1>おはよう</h1>
       <div className="grid grid-cols-3 gap-4">
-        {posts.map((post: Post) => (
-          <div key={post.id} className="bg-amber-200 p-6">
-            <h2 className="text-2xl font-bold">{post.title}</h2>
-            <p>{post.body}</p>
-          </div>
-        ))}
+        {/* {posts.map((post) => ( */}
+        <div key={posts.id} className="bg-amber-200 p-6">
+          <h2 className="text-2xl font-bold mb-3">{posts.address}</h2>
+        </div>
+        {/* ))} */}
       </div>
     </div>
   );
