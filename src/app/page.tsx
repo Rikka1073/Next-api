86de561d68714c4c392fd0f0a4c66a65d86a24ae
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-05-03 16:55:34 +0900

    lodashのインストール

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,38 @@
 export default async function Home() {
-  const data = await fetch("https://api.football-data.org/v4/teams/64", {
+  const fetchData = await fetch("https://api.football-data.org/v4/competitions/PL", {
     headers: {
       "X-Auth-Token": process.env.NEXT_PUBLIC_FOOTBALL_API_KEY || "",
     },
   });
 
-  const posts = await data.json();
-  console.log(posts);
-
-  // type Post = {
-  //   id: number;
-  //   title: string;
-  //   body: string;
-  // };
+  const data = await fetchData.json();
+  const newData = { ...data };
+  console.log(newData);
+  newData.seasons[0].winner.name = "Liverpool FC";
+  // console.log(newData);
+  // console.log(data);
+  // console.log(Array.isArray(data));
+  // console.log(Array.isArray(data.seasons));
+  // console.log(data.seasons);
+  // console.log(data.seasons.winner);
 
   return (
     <div>
-      <h1>おはよう</h1>
-      <div className="grid grid-cols-3 gap-4">
-        {/* {posts.map((post) => ( */}
-        <div key={posts.id} className="bg-amber-200 p-6">
-          <h2 className="text-2xl font-bold mb-3">{posts.address}</h2>
+      <div>
+        <div key={data.id} className="bg-red-500 p-6">
+          <h2 className="text-2xl font-bold mb-3">{data.name}</h2>
+          <div>
+            {data.seasons.map((seasons) => (
+              <div key={seasons.id}>
+                <div className="flex  gap-4">
+                  <p>{seasons.startDate}</p>
+                  <p>{seasons.endDate}</p>
+                </div>
+                {seasons.winner ? <p>{seasons.winner.name}</p> : <p>No winner</p>}
+              </div>
+            ))}
+          </div>
         </div>
-        {/* ))} */}
       </div>
     </div>
   );
