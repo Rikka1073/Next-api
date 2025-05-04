88a9632c29411903db0544145e14d12314ebc1a5
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-05-04 23:02:40 +0900

    オブジェクト変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 export default async function Home() {
   const fetchData = await fetch("https://api.football-data.org/v4/competitions/PL", {
     headers: {
@@ -6,23 +8,62 @@ export default async function Home() {
   });
 
   const data = await fetchData.json();
-  const newData = { ...data };
-  console.log(newData);
-  newData.seasons[0].winner.name = "Liverpool FC";
+  const newData = _.cloneDeep(data);
   // console.log(newData);
+  if (newData.seasons[0].winner === null) {
+    newData.seasons[0].winner = {
+      address: "Anfield Road Liverpool L4 0TH",
+      clubColors: "Red / White",
+      crest: "https://crests.football-data.org/64.png",
+      founded: 1892,
+      id: 64,
+      lastUpdated: "2022-02-10T19:30:22Z",
+      name: "Liverpool FC",
+      shortName: "Liverpool",
+      tla: "LIV",
+      venue: "Anfield",
+      website: "http://www.liverpoolfc.tv",
+    };
+  }
+  console.log(newData);
   // console.log(data);
   // console.log(Array.isArray(data));
   // console.log(Array.isArray(data.seasons));
   // console.log(data.seasons);
-  // console.log(data.seasons.winner);
+  // console.log(newData.seasons[0].winner);
+  // console.log(newData);
 
+  // const object = { name: "soccer" };
+  // const newObject = { ...object };
+  // newObject.name = "baseball";
+
+  // const object = { name: "soccer" };
+  // const newObject = { ...object };
+  // newObject.name = "baseball";
+
+  const object = {
+    name: "soccer",
+    like: {
+      team: "yokohama",
+    },
+  };
+  const newObject = { ...object };
+
+  newObject.name = "baseball";
+  newObject.like.team = "losangeles";
+
+  console.log(object); // { name: "soccer", like: { team: "yokohama" } }
+  console.log(newObject); // { name: "baseball", like: { team: "losangeles" } }
+
+  console.log(object); // {name: "apple"}
+  console.log(newObject); // {name: "banana"}
   return (
     <div>
       <div>
-        <div key={data.id} className="bg-red-500 p-6">
-          <h2 className="text-2xl font-bold mb-3">{data.name}</h2>
+        <div key={newData.id} className="bg-red-500 p-6">
+          <h2 className="text-2xl font-bold mb-3">{newData.name}</h2>
           <div>
-            {data.seasons.map((seasons) => (
+            {newData.seasons.map((seasons) => (
               <div key={seasons.id}>
                 <div className="flex  gap-4">
                   <p>{seasons.startDate}</p>
